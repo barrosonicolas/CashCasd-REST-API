ddaef82f60d61af1947630cb16ab19815a1ade27
barrosonicolas <barrosownicolas@gmail.com>
2023-11-07 19:31:15 -0300

    update controller, repository, and tests

--- src/main/java/com/project/cashcard/CashCardController.java
+++ src/main/java/com/project/cashcard/CashCardController.java
@@ -1,13 +1,15 @@
 package com.project.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Optional;
+import java.net.URI;
+import java.util.*;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -29,7 +31,12 @@ public class CashCardController {
     }
 
     @PostMapping
-    private ResponseEntity createCashCard() {
-        return null;
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
